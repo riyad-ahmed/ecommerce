c2efd2a16979c1e6d9e31459cb25da9bae53885e
riyad-ahmed <riyadahmed2809@gmail.com>
2024-02-04 14:54:31 +0600

    center

--- src/component/sidebar/pasinetion.js
+++ src/component/sidebar/pasinetion.js
@@ -4,7 +4,7 @@ function Pagination() {
   return (
 
     <>
-      <nav aria-label="Page navigation example text-center">
+      <nav aria-label="Page navigation example" className='text-center'>
         <ul className="inline-flex -space-x-px text-base h-10">
           <li>
             <a href="add" className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
